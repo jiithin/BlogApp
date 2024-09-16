b9d921b299e02f32e99f13744c2ac4ad555deb3f
jiithin <jithijithin9@mail.com>
2024-09-16 11:52:07 +0530

    adding repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-3xl text-red-400'>App</h1>
+    </>
+  )
+}
+
+export default App

5f268eeda2784dbd999628e50dad71363541e973
jiithin <jithijithin9@mail.com>
2024-09-19 10:11:39 +0530

    teat route

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import userRoutes from './Routes/userRoute.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=> {console.log('MongoDB is connected')}).catch((err)=>{});
 
 const app = express();
+
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!');
 });
+
+app.use('/blog/user', userRoutes);

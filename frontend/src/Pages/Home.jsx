24d5db16387dfe598fb09809e43819ec73fe4b12
jiithin <jithijithin9@gmail.com>
2024-10-04 22:59:35 +0530

    -

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Card } from 'flowbite-react'
 import React from 'react'
 
 
@@ -10,8 +11,8 @@ function Home() {
 
 <div className="p-5">
 <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Work fast from anywhere</h5>
-    <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.</p>
+    <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Post fast from anywhere</h5>
+    <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with Devulge on iOS & Android. Download the app today.</p>
 
 </div>
 </div>
@@ -19,7 +20,7 @@ function Home() {
 
 
 
-<div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 p-5">
+<div className="grid grid-cols-3 md:grid-cols-2 gap-4 lg:gap-6 p-5">
 
     <div className="grid gap-4">
         
@@ -27,8 +28,9 @@ function Home() {
 
 
 <div
-  className="card shadow-md h-[25em] max-w-full group gap-[0.5em] rounded-xl relative flex justify-end flex-col p-6 z-10 overflow-hidden ">
-  <div className="absolute align-middle top-0 left-0 h-full w-full bg-[url(https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg)] bg-cover group-hover:blur-md"></div>
+  className="card shadow-md h-[25em] max-w-full group gap-[0.5em] rounded-xl relative flex justify-end flex-col p-6 z-10 overflow-hidden  bg-[url(https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg)] bg-cover">
+  <div className="absolute align-middle top-0 left-0 h-full w-full group-hover:backdrop-blur-sm "></div>
+
 
   <div
     className="container text-black z-10 relative font-nunito flex flex-col gap-2"
@@ -67,7 +69,7 @@ function Home() {
     </div>
   </div>
   <p
-    className=" block text-blackfont-light relative h-[0em] group-hover:h-24 leading-[1.2em] duration-500 overflow-hidden"
+    className=" block text-blackfont-light relative h-[0em] group-hover:h-24 leading-[1.2em] duration-500 overflow-hidden text-gray-800"
   >
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet officiis,
     dolorem ab animi magnam culpa fugit error voluptates adipisci, debitis ut
@@ -75,52 +77,24 @@ function Home() {
   </p>
 </div>
 
+<Card
+      className="max-w-sm bg-cover"
+      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgSrc="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+      
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white bg-transparent">
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+    </Card>
 
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
-        </div>
 
 
 
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
-        </div>
+  
     </div>
 </div>
 

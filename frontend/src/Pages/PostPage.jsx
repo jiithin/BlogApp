4711cf019ae0282b85bf784d5aafe032526743fa
jiithin <jithijithin9@gmail.com>
2024-10-11 22:02:50 +0530

    -

--- frontend/src/Pages/PostPage.jsx
+++ frontend/src/Pages/PostPage.jsx
@@ -10,32 +10,7 @@ function PostPage() {
   
 <div className="max-w-screen-xl mx-auto">
   
-    <header className="flex items-center justify-between py-2 border-b">
-      <a href="#" className="px-2 lg:px-0 uppercase font-bold text-purple-800">
-        LOGO
-      </a>
-      <ul className="inline-flex items-center">
-        <li className="px-2 md:px-4">
-          <a href="#" className="text-purple-600 font-semibold hover:text-purple-500"> Home </a>
-        </li>
-        <li className="px-2 md:px-4">
-          <a href="#" className="text-gray-500 font-semibold hover:text-purple-500"> About </a>
-        </li>
-        <li className="px-2 md:px-4">
-          <a href="#" className="text-gray-500 font-semibold hover:text-purple-500"> Press </a>
-        </li>
-        <li className="px-2 md:px-4">
-          <a href="#" className="text-gray-500 font-semibold hover:text-purple-500"> Contact </a>
-        </li>
-        <li className="px-2 md:px-4 hidden md:block">
-          <a href="#" className="text-gray-500 font-semibold hover:text-purple-500"> Login </a>
-        </li>
-        <li className="px-2 md:px-4 hidden md:block">
-          <a href="#" className="text-gray-500 font-semibold hover:text-purple-500"> Register </a>
-        </li>
-      </ul>
-
-    </header>
+   
    
 
     <main className="mt-10">
